0ef19e2c4e2f72770525efba073fd6ff96032f50
4otaku <noname-nya@yandex.ru>
2011-06-18 16:46:11 +0400

    workan

--- engine/modules/draw/output.php
+++ engine/modules/draw/output.php
@@ -7,6 +7,11 @@ class Draw_Output extends Output implements Plugins
 		$vars = Globals::$vars;
 		
 		$this->get_theme($vars['theme']);
+		
+		$this->items['pro'] = ($vars['mode'] == 'shi_painter_pro');
+		$this->items['width'] = (int) $vars['width'];
+		$this->items['height'] = (int) $vars['height'];
+		$this->items['layer_count'] = (int) $vars['layer_count'];
 	}
 	
 	protected function get_theme ($theme_id) {
