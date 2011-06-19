95087c27ed6780f0ebe25dc2ee979182f9627c90
4otaku <noname-nya@yandex.ru>
2011-06-19 23:30:09 +0400

    Больше размеров

--- engine/modules/upload/input.php
+++ engine/modules/upload/input.php
@@ -14,16 +14,53 @@ class Upload_Input extends Input implements Plugins
 		
 		$format = strtolower($image->get_format());
 		$time = time();
-		$save_full = IMAGES.SL.'gallery'.SL.$this->user_id.SL.'full'.SL.$time.'.'.$format;
-		$save_thumb = IMAGES.SL.'gallery'.SL.$this->user_id.SL.'thumb'.SL.$time.'.jpg';
-		$this->test_upload_dirs('gallery', $this->user_id, 'full');
-		$this->test_upload_dirs('gallery', $this->user_id, 'thumb');
 		
-		$thumb_settings = Config::image('thumbnail');
-		$sizes = array($thumb_settings['width'], $thumb_settings['height']);
-		$compression = $thumb_settings['compression'];
-
-		$image->save($save_full)->target($save_thumb)->scale($sizes, $compression, true);
+		$save_full = IMAGES.SL.'gallery'.SL.$this->user_id.SL.'full'.SL.$time.'.'.$format;
+		$save_resized = IMAGES.SL.'gallery'.SL.$this->user_id.SL.'resized'.SL.$time.'.jpg';
+		$save_large_thumbnail = IMAGES.SL.'gallery'.SL.$this->user_id.SL.'large_thumbnail'.SL.$time.'.jpg';
+		$save_thumbnail = IMAGES.SL.'gallery'.SL.$this->user_id.SL.'thumbnail'.SL.$time.'.jpg';		
+		
+		$this->test_upload_dirs('gallery', $this->user_id, 'full');
+		$this->test_upload_dirs('gallery', $this->user_id, 'resized');
+		$this->test_upload_dirs('gallery', $this->user_id, 'large_thumbnail');
+		$this->test_upload_dirs('gallery', $this->user_id, 'thumbnail');		
+		
+		$resized_settings = Config::image('resized');
+		$large_thumbnail_settings = Config::image('large_thumbnail');
+		$thumbnail_settings = Config::image('thumbnail');
+		
+		$image->save($save_full);
+		
+		if (
+			$image->get_width() > $resized_settings['width'] ||
+			$image->get_height() > $resized_settings['height']
+		) {
+			$image->target($save_resized)->scale(
+				array(
+					$resized_settings['width'], 
+					$resized_settings['height']
+				),
+				$resized_settings['compression']
+			);
+		}
+		
+		$image->target($save_large_thumbnail)->scale(
+			array(
+				$large_thumbnail_settings['width'], 
+				$large_thumbnail_settings['height']
+			),
+			$large_thumbnail_settings['compression'],
+			true
+		);
+		
+		$image->target($save_thumbnail)->scale(
+			array(
+				$thumbnail_settings['width'], 
+				$thumbnail_settings['height']
+			),
+			$thumbnail_settings['compression'],
+			true
+		);
 		
 		Art_Input::save($this->user_id, $save_full, $data['timer']);
 		
